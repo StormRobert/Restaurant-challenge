d96676e28eaf2c43a65aa9ffc575f7edf5084f73
StormRobert <stormarasa2@gmail.com>
2023-12-05 11:21:41 +0300

    restaurant added

--- restaurant.py
+++ restaurant.py
@@ -1,5 +1,6 @@
 class Customer:
     customers = []
+
     def __init__(self, given_name, family_name):
         self._given_name = given_name
         self._family_name = family_name
@@ -18,4 +19,21 @@ class Customer:
     @classmethod
     def all(cls):
         return cls.customers
+    
+customer1 = Customer("Storm", "Robert")
+print(customer1.full_name())
+    
+class Restaurant:
+    restaurants = []
 
+    def __init__(self, name):
+        self._name = name
+        Restaurant.restaurants.append(self)
+
+    def name(self):
+        return self._name
+
+    @classmethod
+    def all(cls):
+        return cls.restaurants
+  
