f4557a858b571579e7e842b79ab9b3a5526106c1
StormRobert <stormarasa2@gmail.com>
2023-12-05 12:02:18 +0300

    reviews added

--- restaurant.py
+++ restaurant.py
@@ -37,3 +37,25 @@ class Restaurant:
     def all(cls):
         return cls.restaurants
   
+class Review:
+ reviews = []
+ def __init__(self, customer, restaurant, rating):
+     self._customer = customer
+     self._restaurant = restaurant
+     self._rating = rating
+     Review.reviews.append(self)
+     restaurant.reviews.append(self)
+     customer.reviews.append(self)
+
+def customer(self):
+    return self.customer
+def restaurant(self):
+    return self.restaurant
+def rating(self):
+    return self.rating
+
+@classmethod
+def all(cls):
+    return cls.reviews
+
+
